d342f70313ad8cd7c281c11191828eb243ad2c51
Ando14-Rookie <adrielorlando.advan@gmail.com>
2026-07-21 11:36:20 +0800

    Brought back the exact state of commit 72316b0

--- real_hardware/User/main.c
+++ real_hardware/User/main.c
@@ -1454,6 +1454,7 @@ void save_paint(uint16_t paint_no, color_t * data, uint8_t is_icon) {
     #endif
 }
 
+
 void save_opCode(uint16_t opcode_no, uint8_t * data) {
     if (opcode_no < 0 || opcode_no > page_status_addr_end) {
         #ifdef DEBUG_VERBOSE
